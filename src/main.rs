df70933d73c89d9a2b522917a58237044155a985
Chisenon <kamiru.darkness.contractor@gmail.com>
2024-05-15 18:57:35 +0900

    TailWind理解してきた

--- src/main.rs
+++ src/main.rs
@@ -4,48 +4,28 @@ use yew::{classes, function_component, html, Html};
 fn app() -> Html {
     html! {
         <>
-        //大枠のdiv
-        // <div class={classes!("bg-indigo-300", "text-white", "w-screen")}>
-            
-        //     <h1 class={classes!("text-center", "justify-center", "items-center", "text-[min(13vw,100px)]")}>
-        //         {"こんにちわぁ！！！"}
-        //     </h1>
-
-        //     <img class={classes!()} src="https://static.miraheze.org/bluearchivewiki/3/34/Reisa.png" alt="reisa" />
-            
-        //     <p>{"これ、3個目くらいの環境だったりする。"}</p>
-        //     <p>{"ゆるせん..."}</p>
-        //     <ul>
-        //         <li>{"戦術対抗戦がやばい"}</li>
-        //         <li>{"水ハナコが強すぎる"}</li>
-        //         <li>{"シュン持ってないぞ"}</li>
-        //     </ul>
-        
-        // </div>
-
         // 背景
-        <div class={classes!("h-screen", "w-2/3", "bg-sky-400", "text-center")}>
-            // ヘッダー
-            <div class={classes!("bg-sky-900", "mb-5")}>
-                <p>{"のんのんチャレンジ"}</p>
+        <div class={classes!("flex", "flex-col", "min-h-screen", "justify-between", "text-center", "bg-sky-400")}>
+            <div class={classes!("bg-sky-900")}>
+                <p class={classes!("py-5")}>{"HEADER"}</p>
             </div>
             
             // メインコンテンツ
             <div class={classes!()}>
-                <h1 class={classes!("text-white", "text-[48px]", "font-bold", "mx-auto", )}>
-                    {"のんのんチャレンジ"}
+                <h1 class={classes!("text-white", "text-[72px]", "font-bold", "mx-auto")}>
+                    {"工事中"}
                 </h1>
             </div>
-
+            <div class={classes!()}>
+                <img src="https://pbs.twimg.com/profile_images/1595726694706745344/A2nNf9pH_400x400.jpg" class={classes!("mx-auto")}/>
+            </div>
             // フッター
             <div class={classes!("bg-sky-900")}>
-
+                <h1 class={classes!("py-3")}>{"Made By Chisenon"}</h1>
             </div>
             // <p class={classes!("text-2xl", "flex", "justify-center")}>{"ランダムな平仮名２文字 + のん が10個生成されます！"}</p>
-
             
         </div>
-
         </>
     }
 }
